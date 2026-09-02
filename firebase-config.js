037d4b9ae66d115c55f79e2bae63050d0359c7b0
BotMasterQq <originalldagoplzscan@gmail.com>
2026-09-03 02:57:58 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBayNHn3ndxGmmRDmHW29Vky438gA5Sik0",
+  authDomain: "my-shop-49a5d.firebaseapp.com",
+  projectId: "my-shop-49a5d",
+  storageBucket: "my-shop-49a5d.firebasestorage.app",
+  messagingSenderId: "1046391584002",
+  appId: "1:1046391584002:web:db9fb1115d6a4fe1bffe1b",
+  measurementId: "G-PZ8VPCM08X"
+};
