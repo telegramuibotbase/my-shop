76e75d9d222a64cd0da5b9d7c2745e545756ba0e
BotMasterQq <originalldagoplzscan@gmail.com>
2026-09-03 03:26:28 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,4 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBayNHn3ndxGmmRDmHW29Vky438gA5Sik0",
-  authDomain: "my-shop-49a5d.firebaseapp.com",
-  projectId: "my-shop-49a5d",
-  storageBucket: "my-shop-49a5d.firebasestorage.app",
-  messagingSenderId: "1046391584002",
-  appId: "1:1046391584002:web:db9fb1115d6a4fe1bffe1b",
-  measurementId: "G-PZ8VPCM08X"
+export const supabaseConfig = {
+  url: "https://cbsljlbxzdorzyrxxvoz.supabase.co",
+  anonKey: "sb_publishable_kOwNgUzxwrr4FCqar-51EA_piG20Mx0"
 };
